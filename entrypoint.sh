e4519917f9e1240e4f47eff48a568a280e38c5eb
Anupa Perera <anupa@waan.email>
2024-06-12 14:39:06 +0530

    Update.

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ BUILD_ARGS="$3"
 
 # Run Docker build
 echo "Starting Docker build for Dockerfile: $DOCKERFILE_NAME in path: $DOCKERFILE_PATH"
-if docker build -f "$DOCKERFILE_PATH/$DOCKERFILE_NAME" $BUILD_ARGS -t my-image:latest; then
+if docker build -f "$DOCKERFILE_PATH/$DOCKERFILE_NAME" "$BUILD_ARGS" -t my-image:latest "$DOCKERFILE_PATH"; then
     echo "Docker build completed successfully"
 else
     echo "Docker build failed" >&2
