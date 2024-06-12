d47f84e7ea1829a764ad1cfed9c13effcae64d42
Anupa Perera <anupa@waan.email>
2024-06-12 19:25:19 +0530

    Action update.

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,8 @@ BUILD_ARGS="$3"
 
 # Run Docker build
 echo "Starting Docker build for Dockerfile: $DOCKERFILE_NAME in path: $DOCKERFILE_PATH"
-if docker build -f "$DOCKERFILE_PATH/$DOCKERFILE_NAME" "$BUILD_ARGS" -t my-image:latest "$DOCKERFILE_PATH"; then
+
+if docker build -f "$DOCKERFILE_PATH/$DOCKERFILE_NAME" "$BUILD_ARGS" -t myscan:latest "$DOCKERFILE_PATH"; then
     echo "Docker build completed successfully"
 else
     echo "Docker build failed" >&2
@@ -17,6 +18,7 @@ fi
 
 # Run security scan with Trivy
 echo "Starting security scan with Trivy"
+
 if trivy image my-image:latest; then
     echo "Security scan completed successfully"
 else
